4e6d5ac75454f129b2258bda11414c86807edb32
imBulbi <mlggrizz3@gmail.com>
2016-07-06 13:19:35 -0400

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BeepBoopBot1
 Slack Bot Learning
+wit.ai: Changed Intents to Stories, learn how to apply multiple key words for an intent
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var BotKit = require("botkit")
-var WitBot = require("witbot")
+var BotKit = require("botkit");
+var WitBot = require("witbot");
 
 // Grab SLACK_TOKEN variable in command line
-var SlackToken = process.env.SLACK_TOKEN
-var WitToken = process.env.WIT_TOKEN
+var SlackToken = process.env.SLACK_TOKEN;
+var WitToken = process.env.WIT_TOKEN;
 
 // Create botkit controller
 var controller = BotKit.slackbot({
@@ -17,13 +17,13 @@ controller.spawn({
 // Pass token and start realtime messaging
 }).startRTM(function(err, bot, payload) {
   if (err) {
-    throw new Error("Error connecting to slack: ", err)
+    throw new Error("Error connecting to slack: ", err);
   }
   // Successfully connected
-  console.log("Connected to Slack")
+  console.log("Connected to Slack");
 });
 // SLACK_TOKEN: Found in API bot manage of Slack
 // BASH: SLACK_TOKEN=TOKEN_FOR_BOT_GENERATED npm start
 
 // Instantiate WitBot from wit.ai
-var activeWitBot = WitBot(WitToken)
+var activeWitBot = WitBot(WitToken);
